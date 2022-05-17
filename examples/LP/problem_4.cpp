822f111033ca0b6c7c53b6c12be4aaba97a13c57
vistart <i@vistart.me>
2022-05-17 16:05:01 +0800

    更新示例。

--- examples/LP/problem_4.cpp
+++ examples/LP/problem_4.cpp
@@ -15,7 +15,7 @@
 static SCIP_LPI* lpi = NULL;
 static SCIP_MESSAGEHDLR* messagehdlr = NULL;
 
-SCIP_Bool initProb() {
+SCIP_RETCODE setup() {
     /* this is necessary because the theories don't setup and teardown after each call, but once before and after */
     if (lpi != NULL)
     {
@@ -31,6 +31,25 @@ SCIP_Bool initProb() {
 
     /* This name is necessary because if CPLEX reads a problem from a file its problemname will be the filename. */
     SCIP_CALL(SCIPlpiCreate(&lpi, messagehdlr, "lpi_change_test_problem.lp", SCIP_OBJSEN_MAXIMIZE));
+    return SCIP_OKAY;
+}
+
+SCIP_RETCODE teardown() {
+    assert(!SCIPlpiWasSolved(lpi));
+    SCIP_CALL(SCIPlpiFree(&lpi));
+
+    if (messagehdlr != NULL)
+    {
+        SCIP_CALL(SCIPmessagehdlrRelease(&messagehdlr));
+    }
+    long long mu = BMSgetMemoryUsed();
+    if (mu > 0) {
+        printf("There is a memory leak! Actual %lld\n", mu);
+        BMSdisplayMemory();
+    }
+    else {
+        printf("There is no memory leak.\n");
+    }
     return SCIP_OKAY;
 }
 
@@ -55,19 +74,16 @@ SCIP_RETCODE execmain_test1(int argc, const char** argv) {
     int j;
 
     /* create original lp */
-    SCIP_CALL(SCIPlpiAddCols(lpi, 5, obj, lb, ub, NULL, 0, NULL, NULL, NULL));
+    SCIP_CALL(SCIPlpiAddCols(lpi, 5, obj, lb, ub, NULL, 0, NULL, NULL, NULL)); SCIPlpiGetInternalStatus(lpi);
     SCIP_CALL(SCIPlpiGetNCols(lpi, &ncolsbefore));
-
     for (i = 0; i < iterations; i++)
     {
-        /* setup row values */
         int nrows;
         int nnonzsbefore;
         int nnonzsafter;
 
         nrows = k[i];
 
-        /* get data before modification */
         SCIP_CALL(SCIPlpiGetNNonz(lpi, &nnonzsbefore));
         SCIP_CALL(SCIPlpiGetNRows(lpi, &nrowsbefore));
 
@@ -76,7 +92,7 @@ SCIP_RETCODE execmain_test1(int argc, const char** argv) {
             SCIP_CALL(SCIPlpiDelRows(lpi, 0, -(1 + nrows)));
         }
         else
-        {  /* nrows >= 0 */
+        {
             SCIP_Real lhs[100];
             SCIP_Real rhs[100];
             int beg[100];
@@ -108,72 +124,54 @@ SCIP_RETCODE execmain_test1(int argc, const char** argv) {
                 ind[j] = indvals[j];
                 val[j] = vals[j];
             }
-            SCIP_CALL(SCIPlpiAddRows(lpi, nrows, lhs, rhs, NULL, nnonz, beg, ind, val));
+            SCIP_CALL(SCIPlpiAddRows(lpi, nrows, lhs, rhs, NULL, nnonz, beg, ind, val)); SCIPlpiGetInternalStatus(lpi);
 
-            /* checks */
             SCIP_CALL(SCIPlpiGetRows(lpi, nrowsbefore, nrowsbefore - 1 + nrows, newlhs, newrhs, &newnnonz, newbeg, newind, newval));
             assert(nnonz == newnnonz);
-            //cr_assert_eq(nnonz, newnnonz, "expecting %d, got %d\n", nnonz, newnnonz);
             SCIPdebugMessage("`beg` and `newbeg` should be equal.\n");
             for (int i = 0; i < nrows; i++) {
                 SCIPdebugMessage("row[%d]: (beg, newbeg), (%d, %d)\n", i, beg[i], newbeg[i]);
                 assert(beg[i] == newbeg[i]);
             }
-            //cr_assert_arr_eq(beg, newbeg, nrows * sizeof(int));
 
             beg[nrows] = nnonz;
             newbeg[nrows] = newnnonz;
 
-            /* check each row seperately */
             for (j = 0; j < nrows; j++)
             {
                 cr_assert_float_eq_inf(lhs[j], newlhs[j], 1e-16);
                 cr_assert_float_eq_inf(rhs[j], newrhs[j], 1e-16);
 
-                /* We add a row where the indices are not sorted, some lp solvers give them back sorted (e.g. soplex), some others don't (e.g. cplex).
-                 * Therefore we cannot simply assert the ind and val arrays to be equal, but have to search for and check each value individually. */
                 for (indold = beg[j]; indold < beg[j + 1]; indold++)
                 {
                     int occurrences = 0;
 
-                    /* for each value ind associated to the current row search for it in the newind array */
                     for (indnew = beg[j]; indnew < beg[j + 1]; indnew++)
                     {
                         if (ind[indold] == newind[indnew])
                         {
                             occurrences = occurrences + 1;
                             assert(ABS(val[indold] - newval[indnew]) < 1e-16);
-                            //cr_assert_float_eq(val[indold], newval[indnew], 1e-16, "expected %g got %g\n", val[indold], newval[indnew]);
                         }
                     }
-                    /* assert that we found only one occurrence in the current row */
                     assert(occurrences == 1);
-                    //cr_assert_eq(occurrences, 1);
                 }
             }
         }
 
-        /* checks */
         SCIP_CALL(SCIPlpiGetNRows(lpi, &nrowsafter));
         assert(nrowsbefore + nrows == nrowsafter);
-        //cr_assert_eq(nrowsbefore + nrows, nrowsafter);
 
         SCIP_CALL(SCIPlpiGetNNonz(lpi, &nnonzsafter));
         SCIPdebugMessage("nnonzsbefore %d, nnonzsafter %d, nnonzsdiff[i] %d, in iteration %d\n", nnonzsbefore, nnonzsafter, nnonzsdiff[i], i);
         assert(nnonzsbefore + nnonzsdiff[i] == nnonzsafter);
-        //cr_assert_eq(nnonzsbefore + nnonzsdiff[i], nnonzsafter, "nnonzsbefore %d, nnonzsafter %d, nnonzsdiff[i] %d, in iteration %d\n",
-        //    nnonzsbefore, nnonzsafter, nnonzsdiff[i], i);
 
         SCIP_CALL(SCIPlpiGetNCols(lpi, &ncolsafter));
         assert(ncolsbefore == ncolsafter);
-        //cr_assert_eq(ncolsbefore, ncolsafter);
     }
 
-    /* delete rowsets */
-    /* should have 8 rows now */
     SCIP_CALL(SCIPlpiGetNRows(lpi, &nrowsbefore));
     assert(8 == nrowsbefore);
-    //cr_assert_eq(8, nrowsbefore);
     for (i = 3; i > 0; i--)
     {
         int rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
@@ -182,11 +180,9 @@ SCIP_RETCODE execmain_test1(int argc, const char** argv) {
             rows[(2 * j) + 1] = 1;
 
         SCIP_CALL(SCIPlpiGetNRows(lpi, &nrowsbefore));
-        SCIP_CALL(SCIPlpiDelRowset(lpi, rows));
+        SCIP_CALL(SCIPlpiDelRowset(lpi, rows)); SCIPlpiGetInternalStatus(lpi);
         SCIP_CALL(SCIPlpiGetNRows(lpi, &nrowsafter));
         assert(nrowsbefore - i == nrowsafter);
-        //cr_assert_eq(nrowsbefore - i, nrowsafter);
-        /* assert that the rows that are left are the ones I intended */
     }
     return SCIP_OKAY;
 }
@@ -217,6 +213,7 @@ SCIP_RETCODE execmain_test2(int argc, const char** argv) {
 
     for (i = 0; i < iterations; i++)
     {
+        BMSdisplayMemory();
         /* setup col values */
         int ncols;
         int nnonzsbefore;
@@ -231,6 +228,7 @@ SCIP_RETCODE execmain_test2(int argc, const char** argv) {
         if (ncols < 0)
         {
             SCIP_CALL(SCIPlpiDelCols(lpi, 0, -(1 + ncols)));
+            BMSdisplayMemory();
         }
         else
         {  /* ncols >= 0 */
@@ -265,6 +263,7 @@ SCIP_RETCODE execmain_test2(int argc, const char** argv) {
             }
             //printf("ncols, j: %d, %d\n", ncols, j);
             SCIP_CALL(SCIPlpiAddCols(lpi, ncols, obj, lb, ub, NULL, nnonz, beg, ind, val));
+            BMSdisplayMemory();
 
             /* checks */
             SCIP_CALL(SCIPlpiGetCols(lpi, ncolsbefore, ncolsbefore - 1 + ncols, newlb, newub, &newnnonz, newbeg, newind, newval));
@@ -325,83 +324,165 @@ SCIP_RETCODE execmain_test2(int argc, const char** argv) {
 }
 
 SCIP_RETCODE execmain_test3(int argc, const char** argv) {
-    int nrows, ncols, nnonz;
-    SCIP_Real objval;
-    SCIP_Real primsol[2];
-    SCIP_Real dualsol[2];
-    SCIP_Real activity[2];
-    SCIP_Real redcost[2];
-    SCIP_Real objval2;
-    SCIP_Real primsol2[2];
-    SCIP_Real dualsol2[2];
-    SCIP_Real activity2[2];
-    SCIP_Real redcost2[2];
-    SCIP_OBJSEN sense;
+    int nrows;
+    int ncols;
+    int beg = 0;
+    SCIP_Real lb = 0.0;
+    SCIP_Real ub = 3.0;
+    SCIP_Real lhs = 1.0;
+    SCIP_Real rhs = 2.0;
+    SCIP_Real obj = 1.0;
+    SCIP_Real val = 1.0;
+    int ind = 0;
 
-    ncols = 2;
-    nrows = 2;
-    nnonz = 2;
-    sense = SCIP_OBJSEN_MAXIMIZE;
+    //lpi = NULL;
 
-    const char* rownames[] = { "row1", "row2" };
-    const char* colnames[] = { "x1", "x2" };
-    SCIP_Real obj[100] = { 1.0, 1.0 };
-    SCIP_Real  lb[100] = { 0.0, 0.0 };
-    SCIP_Real  ub[100] = { SCIPlpiInfinity(lpi),  SCIPlpiInfinity(lpi) };
-    SCIP_Real lhs[100] = { -SCIPlpiInfinity(lpi), -SCIPlpiInfinity(lpi) };
-    SCIP_Real rhs[100] = { 1.0, 1.0 };
-    SCIP_Real val[100] = { 1.0, 1.0 };
-    int beg[100] = { 0, 1 };
-    int ind[100] = { 0, 1 };
+    /* create LPI */
+    //SCIP_CALL(SCIPlpiCreate(&lpi, NULL, "prob", SCIP_OBJSEN_MAXIMIZE));
 
+    /* use the following LP as base:
+     *   max x
+     *       1 <= x <= 2  (linear constraint)
+     *       0 <= x <= 3  (bounds)
+     */
+     /* add one column */
+    SCIP_CALL(SCIPlpiAddCols(lpi, 1, &obj, &lb, &ub, NULL, 0, NULL, NULL, NULL));
 
+    /* add one row */
+    SCIP_CALL(SCIPlpiAddRows(lpi, 1, &lhs, &rhs, NULL, 1, &beg, &ind, &val));
 
-    SCIP_CALL(SCIPlpiChgObjsen(lpi, sense));
-    SCIP_CALL(SCIPlpiAddCols(lpi, ncols, obj, lb, ub, (char**)colnames, 0, NULL, NULL, NULL));
-    SCIP_CALL(SCIPlpiAddRows(lpi, nrows, lhs, rhs, (char**)rownames, nnonz, beg, ind, val));
-    assert(!SCIPlpiWasSolved(lpi));
+    /* check size */
+    SCIP_CALL(SCIPlpiGetNRows(lpi, &nrows));
+    SCIP_CALL(SCIPlpiGetNCols(lpi, &ncols));
+    assert(nrows == 1);
+    assert(ncols == 1);
+
+#ifdef SCIP_DEBUG
+    /* turn on output */
+    SCIP_CALL(SCIPlpiSetIntpar(lpi, SCIP_LPPAR_LPINFO, 1));
+#endif
+    // unittests-lpi-bases, Test(simple, test1)
+    int cstat;
+    int rstat;
+
+    /* solve problem */
     SCIP_CALL(SCIPlpiSolvePrimal(lpi));
-    assert(SCIPlpiWasSolved(lpi));
 
-    SCIP_CALL(SCIPlpiSolvePrimal(lpi));
-    SCIP_CALL(SCIPlpiGetSol(lpi, &objval, primsol, dualsol, activity, redcost));
+    /* get basis */
+    SCIP_CALL(SCIPlpiGetBase(lpi, &cstat, &rstat));
 
-    SCIP_CALL(SCIPlpiWriteLP(lpi, "lpi_change_test_problem.lp"));
-    SCIP_CALL(SCIPlpiClear(lpi));
+    /* the variable should be basic and the slack variable at the upper bound */
+    assert(cstat == SCIP_BASESTAT_BASIC);
+    assert(rstat == SCIP_BASESTAT_UPPER);
 
-    SCIP_CALL(SCIPlpiReadLP(lpi, "lpi_change_test_problem.lp"));
+    SCIP_CALL(SCIPlpiFree(&lpi));
 
-    SCIP_CALL(SCIPlpiSolvePrimal(lpi));
-    SCIP_CALL(SCIPlpiGetSol(lpi, &objval2, primsol2, dualsol2, activity2, redcost2));
-    assert(ABS(objval - objval2) < EPS);
-    //cr_assert_float_eq(objval, objval2, EPS);
-    for (int i = 0; i < 2; i++) {
-        assert(primsol[i] == primsol2[i]);
-        assert(dualsol[i] == dualsol2[i]);
-        assert(activity[i] == activity2[i]);
-        assert(redcost[i] == redcost2[i]);
+    if (messagehdlr != NULL)
+    {
+        SCIP_CALL(SCIPmessagehdlrRelease(&messagehdlr));
     }
-    /**
-    cr_assert_arr_eq(primsol, primsol2, 2 * sizeof(SCIP_Real));
-    cr_assert_arr_eq(dualsol, dualsol2, 2 * sizeof(SCIP_Real));
-    cr_assert_arr_eq(activity, activity2, 2 * sizeof(SCIP_Real));
-    cr_assert_arr_eq(redcost, redcost2, 2 * sizeof(SCIP_Real));*/
+    long long mu = BMSgetMemoryUsed();
+    if (mu > 0) {
+        printf("There is a memory leak! Actual %lld\n", mu);
+        BMSdisplayMemory();
+    }
+    else {
+        printf("There is no memory leak.\n");
+    }
+
+    return SCIP_OKAY;
+}
+
+SCIP_RETCODE execmain_test4(int argc, const char** argv) {
+    int ncols;
+    int beg = 0;
+    int inds[2];
+    int nrows;
+    SCIP_Real vals[2];
+    SCIP_Real lb;
+    SCIP_Real ub;
+    SCIP_Real obj;
+    SCIP_Real lhs;
+    SCIP_Real rhs;
+
+    /* create LPI */
+    //SCIP_CALL(SCIPlpiCreate(&lpi, NULL, "prob", SCIP_OBJSEN_MAXIMIZE));
+
+    /* use the following LP:
+     * max 1 x1 + 1 x2 + 1 x3
+     *       -8 <= -x1 -          x3 <= -1
+     *       -7 <= -x1 -   x2        <= -1
+     *              x1 + 2 x2        <= 12
+     *              x1,    x2,    x3 >= 0
+     */
+     /* add columns */
+    lb = 0.0;
+    ub = SCIPlpiInfinity(lpi);
+    obj = 1.0;
+
+    SCIP_CALL(SCIPlpiAddCols(lpi, 1, &obj, &lb, &ub, NULL, 0, NULL, NULL, NULL));
+    SCIP_CALL(SCIPlpiAddCols(lpi, 1, &obj, &lb, &ub, NULL, 0, NULL, NULL, NULL));
+    SCIP_CALL(SCIPlpiAddCols(lpi, 1, &obj, &lb, &ub, NULL, 0, NULL, NULL, NULL));
+
+    /* add rows */
+    lhs = -8.0;
+    rhs = -1.0;
+    inds[0] = 0;
+    inds[1] = 2;
+    vals[0] = -1.0;
+    vals[1] = -1.0;
+    SCIP_CALL(SCIPlpiAddRows(lpi, 1, &lhs, &rhs, NULL, 2, &beg, inds, vals));
+
+    lhs = -7.0;
+    rhs = -1.0;
+    inds[0] = 0;
+    inds[1] = 1;
+    vals[0] = -1.0;
+    vals[1] = -1.0;
+    SCIP_CALL(SCIPlpiAddRows(lpi, 1, &lhs, &rhs, NULL, 2, &beg, inds, vals));
+
+    lhs = -SCIPlpiInfinity(lpi);
+    rhs = 12.0;
+    inds[0] = 0;
+    inds[1] = 1;
+    vals[0] = 1.0;
+    vals[1] = 2.0;
+    SCIP_CALL(SCIPlpiAddRows(lpi, 1, &lhs, &rhs, NULL, 2, &beg, inds, vals));
+
+    /* check size */
+    SCIP_CALL(SCIPlpiGetNRows(lpi, &nrows));
+    SCIP_CALL(SCIPlpiGetNCols(lpi, &ncols));
+    assert(nrows == 3);
+    assert(ncols == 3);
+
+#ifdef SCIP_DEBUG
+    /* turn on output */
+    SCIP_CALL(SCIPlpiSetIntpar(lpi, SCIP_LPPAR_LPINFO, 1));
+#endif
     return SCIP_OKAY;
 }
 
 int main(int argc, const char* argv[]) {
     printf("Hello, SCIP! This problem would be solved by using SCIP integrated with SCS.\n");
-    if (!initProb()) {
-        printf("The problem failed to initialize.\n");
+    if (!setup()) {
+        printf("The test 1 failed to initialize.\n");
     }
     if (execmain_test1(argc, argv) == SCIP_OKAY) {
         printf("The test 1 passed!\n");
     }
-    if (!initProb()) {
-        printf("The problem failed to initialize.\n");
+    teardown();
+    if (!setup()) {
+        printf("The test 2 failed to initialize.\n");
     }
     if (execmain_test2(argc, argv) == SCIP_OKAY) {
         printf("The test 2 passed!\n");
+    }
+    teardown();
+    if (!setup()) {
+        printf("The test 3 failed to initialize.\n");
+    }
+    if (execmain_test3(argc, argv) == SCIP_OKAY) {
+        printf("The test 3 passed!\n");
     }
     return 0;
 }
